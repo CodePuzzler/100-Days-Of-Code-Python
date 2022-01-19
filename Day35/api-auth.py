60d0a81f1db872b0321d60679b30699e8af45568
Amit Yadav <ay.puzzler@gmail.com>
2022-01-19 16:44:08 +0530

    API auth, Weather Data, Rain alert

---
+++ Day35/api-auth.py
@@ -0,0 +1,20 @@
+# Day35 of my 100DaysOfCode Challenge
+# Example of API key authentication using open weather API
+import requests
+
+# authentication key
+API_KEY = "b3b854a59e0c8da32cf8600b2782ca3a"
+OWN_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+
+weather_params = {
+    "lat": 28.585230,
+    "lon": 76.374611,
+    "appid": API_KEY,
+    "exclude": "current,minutely,daily",
+}
+
+response = requests.get(OWN_Endpoint, params=weather_params)
+weather_data = response.json()
+
+print(weather_data)
+
